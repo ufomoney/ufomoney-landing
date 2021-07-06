f851f1c8c20cba1db67889b149acd862cd84a1fb
maxrobinso <maxrobinsons.inbox@gmail.com>
2021-07-06 18:44:22 +0300

    first

---
+++ src/utils/menu.js
@@ -0,0 +1,103 @@
+const menu = [
+  {
+    name: 'Products',
+    sublinks: [
+      {
+        name: 'App',
+        link: 'https://app.uniswap.org/#/',
+        description: 'Swap tokens and supply liquidity'
+      },
+      {
+        name: 'Analytics',
+        link: 'https://uniswap.info/',
+        description: 'Uniswap analytics and historical data'
+      },
+      {
+        name: 'Token Lists',
+        link: 'https://tokenlists.org/',
+        description: 'A new Ethereum token list standard.'
+      },
+      {
+        name: 'Unisocks',
+        link: 'https://unisocks.exchange/',
+        description: 'Dynamically priced socks'
+      },
+      {
+        name: 'Sybil',
+        link: 'https://sybil.org/#/',
+        description: 'Identities for governance'
+      },
+      {
+        name: 'Unipig',
+        link: 'https://unipig.exchange/',
+        description: 'Optimistic rollup demo'
+      }
+    ]
+  },
+  {
+    name: 'Developers',
+    sublinks: [
+      {
+        name: 'V3 Documentation',
+        link: 'https://docs.uniswap.org/'
+      },
+      { name: 'V3 Whitepaper', link: '/whitepaper-v3.pdf' },
+      { name: 'GitHub', link: 'https://github.com/Uniswap' },
+      // { name: 'Audit', link: '/audit.html' },
+      { name: 'Bug Bounty', link: '/bug-bounty' },
+      { name: '―', link: '/' },
+      {
+        name: 'V2 Documentation',
+        link: '/docs/v2'
+      },
+      { name: 'V2 Whitepaper', link: '/whitepaper.pdf' }
+    ]
+  },
+  {
+    name: 'Governance',
+    sublinks: [
+      {
+        name: 'UNI Token',
+        link: '/blog/uni'
+      },
+      { name: 'Forum', link: 'https://gov.uniswap.org/' },
+      { name: 'Voting Portal', link: 'https://app.uniswap.org/#/vote' },
+      { name: 'Documentation', link: '/docs/v2/governance/overview' }
+    ]
+  },
+  {
+    name: 'Community',
+    sublinks: [
+      { name: 'Discord', link: 'https://discord.gg/FCfyBSbCU5' },
+      { name: 'Twitter', link: 'https://twitter.com/Uniswap' },
+      { name: 'Reddit', link: 'https://www.reddit.com/r/Uniswap' }
+    ]
+  },
+  {
+    name: 'About',
+    sublinks: [
+      {
+        name: 'Blog',
+        link: '/blog'
+      },
+      {
+        name: 'Info',
+        link: '/about'
+      },
+      {
+        name: 'FAQ',
+        link: '/faq'
+      },
+      {
+        name: 'Jobs',
+        link: '/about#jobs'
+      },
+      {
+        name: 'Logo & Brand',
+        link: '/about#brand'
+      }
+    ]
+  }
+]
+
+module.exports = menu
